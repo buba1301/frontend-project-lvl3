194753d22da2de0af89e5905e8cf9ffa31790ebd
Mona Lisa <bmuza57@gmail.com>
2020-04-15 00:21:41 +0300

    fix html

--- webpack.config.js
+++ webpack.config.js
@@ -10,7 +10,7 @@ module.exports = {
   },
   output: {
     path: path.resolve(__dirname, './dist'),
-    filename: 'app.js',
+    filename: '[name].js',
     publicPath: '/dist',
   },
   devServer: {
